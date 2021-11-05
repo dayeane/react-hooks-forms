c45b187b954083b5c898cdd91b65f7e8020d26db
Dayeane <dayeane85@gmail.com>
2021-11-04 21:46:52 -0700

    controlled components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 
 function Form() {
+
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+  
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input type="text"  onChange={handleFirstNameChange} value={firstName} />
+      <input type="text"  onChange={handleLastNameChange}  value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
